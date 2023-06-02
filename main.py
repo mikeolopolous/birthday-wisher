b413b5cf4062bd0a31535040661264295d8ee26a
Miguel Lopez <ing.miguell@gmail.com>
2023-06-02 17:32:31 -0600

    add funcionatility

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
-import datetime as dt
-from random import choice
+from datetime import datetime
+from random import randint
+import pandas
 import smtplib
 
 FROM_EMAIL = ""
 PASSWORD = ""
 SMTP_SERVER = "smtp.gmail.com"
-TO_EMAILS = ""
 
-now = dt.datetime.now()
-weekday = now.weekday()
+today = (datetime.now().month, datetime.now().day)
+file_exist = True
 
-if weekday == 3:
-    with open("./data/quotes.txt") as file:
-        quotes = file.readlines()
-        random_quote = choice(quotes)
-    
+try:
+    original_data = pandas.read_csv("./data/birthday.csv")
+except FileNotFoundError:
+    file_exist = False
+
+    default = {
+        'name': pandas.Series('Miguel Lopez'),
+        'email': pandas.Series(FROM_EMAIL),
+        'month': pandas.Series(today[0]),
+        'day': pandas.Series(today[1])
+    }
+
+    msg_default = "Please, check the csv file at 'Birthday-wisher project'."
+
+    data = pandas.DataFrame(default)
+    birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+else:
+    birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in original_data.iterrows()}
+
+if today in birthdays_dict:
+    template_path = f"./letter_templates/letter_{randint(1, 3)}.txt"
+    with open(template_path) as template_file:
+        letter = template_file.read()
+        letter = letter.replace("[NAME]", birthdays_dict[today]["name"])
+
     with smtplib.SMTP(SMTP_SERVER) as connection:
         connection.starttls()
         connection.login(user=FROM_EMAIL, password=PASSWORD)
         connection.sendmail(
             from_addr=FROM_EMAIL,
-            to_addrs=TO_EMAILS,
-            msg=f"Subject:Be positive!\n\n{random_quote}"
+            to_addrs=birthdays_dict[today]["email"],
+            msg=f"Subject:Congratulations!\n\n{letter}" if file_exist else f"Subject:Error!\n\n{msg_default}"
         )
